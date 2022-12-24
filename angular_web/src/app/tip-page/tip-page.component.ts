017da6ac0e8cdb896023480f0ec7ab72709b0c9f
franky3020 <210238hwsh@gmail.com>
2022-12-25 01:09:58 +0800

    + p5 進去

--- angular_web/src/app/tip-page/tip-page.component.ts
+++ angular_web/src/app/tip-page/tip-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as p5 from 'p5';
 
 @Component({
   selector: 'app-tip-page',
@@ -7,4 +8,34 @@ import { Component } from '@angular/core';
 })
 export class TipPageComponent {
 
+  ngOnInit() {
+    const s = (sketch: any) => {
+
+      sketch.setup = () => {
+
+        let width = 300;
+        let height = 300;
+
+        let magicCloth = sketch.createCanvas(width, height);
+        magicCloth.parent('magicCloth');
+
+        sketch.background(0);
+
+      };
+
+      sketch.draw = () => {
+        // if (magicControl.canStartShow && sketch.mouseIsPressed) {
+        //   sketch.erase();
+        //   sketch.ellipse(sketch.mouseX, sketch.mouseY, 80, 80);
+        // }
+
+        if (sketch.mouseIsPressed) {
+          sketch.erase();
+          sketch.ellipse(sketch.mouseX, sketch.mouseY, 80, 80);
+        }
+      };
+    };
+
+    new p5(s);
+  }
 }
