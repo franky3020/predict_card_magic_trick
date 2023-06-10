f891c38d0c2783a53fdd960f82b30ecce13c6af5
franky3020 <210238hwsh@gmail.com>
2023-06-10 22:01:21 +0800

    新增app store link

--- angular_web/src/app/home-page/home-page.component.ts
+++ angular_web/src/app/home-page/home-page.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { LocalStorageService } from '../local-storage.service';
 declare var cordova: any;
+declare var device: any;
+
+const googlePlayLink = 'https://play.google.com/store/apps/details?id=tw.franky.predict_card';
+const appStoreLink = 'https://apps.apple.com/us/app/predict-card-magic-trick/id6445894214';
 
 @Component({
   selector: 'app-home-page',
@@ -11,13 +15,31 @@ export class HomePageComponent {
 
 
   isUserLearned = false;
+  appLink = googlePlayLink;
+
 
   constructor(
     private localStorageService: LocalStorageService
-  ) {}
+  ) {
+    document.addEventListener("deviceready", this.updateAppLink, false);
+  }
 
   ngOnInit() {
     this.isUserLearned = this.localStorageService.isUserLearned();
+  }
+
+  goToAppStroe() {
+    window.open(this.appLink, '_blank');
+  }
+
+  updateAppLink() {
+    if (device) {
+      if (device.platform === 'Android') {
+        this.appLink = googlePlayLink;
+      } else if (device.platform === 'iOS') {
+        this.appLink = appStoreLink;
+      }
+    }
   }
   
 }
