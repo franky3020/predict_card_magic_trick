340742541ab0eafa4f2a8378e7a3fb924137a25f
franky3020 <210238hwsh@gmail.com>
2023-09-04 11:49:33 +0800

    add docker back-end

--- back-server/index.js
+++ back-server/index.js
@@ -1,20 +1,37 @@
-const express = require('express')
-const cors = require('cors')
+const express = require("express");
+const cors = require("cors");
+const https = require("https");
+const fs = require("fs");
 
 const versionList = require("./version.json");
 
-const app = express()
-app.use(cors())
-const port = 3000
+const app = express();
+app.use(cors());
+const port = 3000;
 
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
+app.get("/appVersion", (req, res) => {
+  res.send(versionList);
+});
 
-app.get('/appVersion', (req, res) => {
-  res.send(versionList)
-})
+if(process.env.NODE_ENV === "https") {
+  https
+  .createServer(
+    // Provide the private and public key to the server by reading each
+    // file's content with the readFileSync() method.
+    {
+      key: fs.readFileSync("frankyya_com.p7b"),
+      cert: fs.readFileSync("frankyya_com.crt"),
+    },
+    app
+  )
+  .listen(port, () => {
+    console.log(`[https]: serever is runing at port ${port}`);
+  });
+} else {
+  app.listen(port, () => {
+    console.log(`[http]: serever is runing at port ${port}`);
+  });
+}
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+
+
